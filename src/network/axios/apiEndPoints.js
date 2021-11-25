7e89734bf0eac9cb500d8ccbf59d72951cdcade5
Faddal Ibrahim <faddalibrahim@gmail.com>
2021-11-25 00:01:37 +0000

    we are deploying

--- src/network/axios/apiEndPoints.js
+++ src/network/axios/apiEndPoints.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "http://20.108.171.90/auto-cv-backend/api/";
-export const STUDENT_LOGIN = "student/login.php";
-export const STUDENT_REGISTER = "student/register.php";
-export const STUDENT_FORGOT_PASSWORD = "student/forgot-password.php";
-export const STUDENT_RESET_PASSWORD = "student/reset-password.php";
+export const STUDENT_LOGIN = "student/login.php/";
+export const STUDENT_REGISTER = "student/register.php/";
+export const STUDENT_FORGOT_PASSWORD = "student/forgot-password.php/";
+export const STUDENT_RESET_PASSWORD = "student/reset-password.php/";
